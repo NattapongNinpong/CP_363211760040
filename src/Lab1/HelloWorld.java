ab1f482fc4b62b92d1fd8f768bbf82069aed995d
NattapongNinpong <nattapong.ni@rmutsvmail.com>
2020-07-15 14:56:15 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("Nattapong Ninpong");
+        System.out.println("363211760040");
+        System.out.println("คณะ เทคโนโลยีการจัดการ");
+        System.out.println("สาขา การจัดเทคโนโลยีสารสนเทศ");
+
 
 
 
