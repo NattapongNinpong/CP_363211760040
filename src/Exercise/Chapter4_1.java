4451807877a30c71e966b0b2aa6270157823f81e
NattapongNinpong <nattapong.ni@rmutsvmail.com>
2020-08-05 15:46:24 +0700

    Chapter4_1-3_Example_if-if_else-fi_else_fi

--- src/Exercise/Chapter4_1.java
+++ src/Exercise/Chapter4_1.java
@@ -1,9 +1,38 @@
 package Exercise;
 
+import sun.plugin2.message.JavaScriptGetWindowMessage;
+
 import java.util.Scanner;
 
 public class Chapter4_1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("num1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("num2: ");
+        int num2 = scanner.nextInt();
+        System.out.print("num3: ");
+        int num3 = scanner.nextInt();
+        System.out.print("num4: ");
+        int num4 = scanner.nextInt();
+        System.out.print("num5: ");
+        int num5 = scanner.nextInt();
+        System.out.print("num6: ");
+        int num6 = scanner.nextInt();
+        System.out.print("num7: ");
+        int num7 = scanner.nextInt();
+        System.out.print("num8: ");
+        int num8 = scanner.nextInt();
+        System.out.print("num9: ");
+        int num9 = scanner.nextInt();
+        System.out.print("num10: ");
+        int num10 = scanner.nextInt();
+
+        int toal =num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+        int avg = toal / 10;
+
+        System.out.println("Total : "+toal);
+        System.out.println("Average : "+avg);
 
     }
 }
---
+++ src/Exercise/Chapter4_2.java
@@ -0,0 +1,33 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Chapter4_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter integer: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter real number: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter integer: ");
+        int c = scanner.nextInt();
+        System.out.print("Enter real number: ");
+        double d = scanner.nextDouble();
+        System.out.print("Enter integer: ");
+        int g = scanner.nextInt();
+        System.out.print("Enter real number: ");
+        double f = scanner.nextDouble();
+        System.out.print("Enter integer: ");
+        int e = scanner.nextInt();
+        System.out.print("Enter real number: ");
+        double y = scanner.nextDouble();
+        System.out.print("Enter integer: ");
+        int t = scanner.nextInt();
+        System.out.print("Enter real number: ");
+        double r = scanner.nextDouble();
+
+        System.out.println(a+" "+b+" "+c+" "+d+" "+g+" "+f+" "+e+" "+t+" "+y+" "+r);
+
+    }
+}
