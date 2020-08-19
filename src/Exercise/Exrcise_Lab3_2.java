615ee4f74e81343c420323bc0cdb8293188c1bc0
NattapongNinpong <nattapong.ni@rmutsvmail.com>
2020-08-19 15:48:22 +0700

    Example

---
+++ src/Exercise/Exrcise_Lab3_2.java
@@ -0,0 +1,14 @@
+package Exercise;
+
+public class Exrcise_Lab3_2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) { //row
+            for (int j = 1; j <=i ; j++) {//column
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+        }
+    }
+}
